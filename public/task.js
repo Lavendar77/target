9b6de2bf2469ad21ac4c43f7471b3b86275f7cee
Adeyinka Adefolurin <folurinyinka@gmail.com>
2023-04-01 19:27:38 +0100

    wip: prepare all rule functions

--- public/task.js
+++ public/task.js
@@ -10,7 +10,11 @@ async function bootTargetScript() {
     await fetch(`${url.origin}/api/domains/${reference}`)
         .then((response) => response.json())
         .then((data) => {
-            applyTargetRulesToApp(data.data.domain.rules);
+            let domain = data.data.domain;
+
+            if (confirmTargetDomain(domain.base_url)) {
+                applyTargetRulesToApp(domain.rules);
+            }
         })
         .catch(() => {
             console.error('Script not installed correctly.')
@@ -18,10 +22,68 @@ async function bootTargetScript() {
 }
 
 /**
+ * Validate the rules are installed on the correct domain.
+ *
+ * @param {string} url
+ * @returns Boolean
+ */
+function confirmTargetDomain(url) {
+    return window.location.host === new URL(url).host;
+}
+
+/**
  * Apply the rules to the page.
  *
- * @param {Array} rules
+ * @param {array} rules
  */
 function applyTargetRulesToApp(rules) {
+    let currentAppPath = window.location.pathname;
 
+    rules.forEach((rule) => {
+        //
+    })
+}
+
+/**
+ * Validate the 'contains' target rule.
+ *
+ * @param {string} path
+ * @param {string} page
+ * @returns Boolean
+ */
+function targetRuleContains(path, page) {
+    return path.includes(page);
+}
+
+/**
+ * Validate the 'exact' target rule.
+ *
+ * @param {string} path
+ * @param {string} page
+ * @returns Boolean
+ */
+function targetRuleExact(path, page) {
+    return path == page;
+}
+
+/**
+ * Validate the 'starts_with' target rule.
+ *
+ * @param {string} path
+ * @param {string} page
+ * @returns Boolean
+ */
+function targetRuleStartsWith(path, page) {
+    return path.startsWith(page);
+}
+
+/**
+ * Validate the 'ends_with' target rule.
+ *
+ * @param {string} path
+ * @param {string} page
+ * @returns Boolean
+ */
+function targetRuleEndsWith(path, page) {
+    return path.endsWith(page);
 }
