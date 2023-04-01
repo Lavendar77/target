7c75f3ec554c9f77ad2c3c246af8a77771d1124c
Adeyinka Adefolurin <folurinyinka@gmail.com>
2023-04-01 09:21:59 +0100

    style: clean the web route
    move callbacks to controller

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class DashboardController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Inertia\Response
+     */
+    public function __invoke(Request $request): Response
+    {
+        return Inertia::render('Dashboard', [
+            'domains' => $request->user()->domains,
+        ]);
+    }
+}
