5034106742bfcffae28186c7d1d6e959da823a2c
Adeyinka Adefolurin <folurinyinka@gmail.com>
2023-04-01 19:27:25 +0100

    fix: remove throttle on API

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('domains/{reference}', DomainController::class);
+Route::get('domains/{reference}', DomainController::class)->withoutMiddleware('throttle');
